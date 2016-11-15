88b664f562c32d2854e0b63b2adf3fe07da03c36
Bang <nkbang@gmail.com>
2016-11-15 10:09:43 +0700

    BT 9

--- include.php
+++ include.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "mypassword", "gis_k22");
 mysqli_set_charset($conn, 'utf8');
-if ($connection->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
 }
 ?>
