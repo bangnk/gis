6622390cca4a90979a1fe1fb0772ffcc77ffd65e
Bang <nkbang@gmail.com>
2016-11-15 23:55:05 +0700

    BT 21

---
+++ bt21.php
@@ -0,0 +1,139 @@
+<?php
+include 'include.php';
+$result = mysqli_query($conn, "SELECT id, lng, lat, title, content FROM markers");
+$emparray = array();
+while($row =mysqli_fetch_assoc($result)) {
+    $emparray[] = $row;
+}
+$conn->close();
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Can Tho city</title>
+  <link type="text/css" href="css/style.css" rel="stylesheet" media="all" />
+  <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" media="all" />
+</head>
+<body>
+<div class="container-fluid">
+    <div class='row header'>
+        <form class="form-inline" id='gis_form'>
+            <div class="form-group">
+                <label for="fromInputText" >From:</label>
+                <input type="text" class="form-control" id="fromInputText" name="from" style='width: 200px;'>
+            </div>
+            <div class="form-group" style='margin-left: 15px;'>
+                <label for="toInputText" >To:</label>
+                <input type="text" class="form-control" id="toInputText" name="to" style='width: 200px;'>
+            </div>
+            <button type="submit" class="btn btn-default" id="sbutton" style='margin-left: 25px;'>Show Route</button>
+            <div class="spinner medium" style='display: none;' id='spinner'></div>
+        </form>
+    </div>
+</div>
+<div id="map" class='has_header'></div>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/map-style.js"></script>
+<script>
+  var infoWindow;
+  var map;
+  var allMarkers = [];
+  function createMarker(info, m, bounds) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(info.lat, info.lng),
+        map: m,
+        title: info.title,
+        icon: 'imgs/logo_ctu.gif'
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.close();
+      infoWindow.setContent(info.content);
+      infoWindow.open(map, marker);
+    });
+    bounds.extend(marker.position);
+    allMarkers.push(marker);
+    return marker;
+  }
+  function initMap() {
+    infoWindow = new google.maps.InfoWindow({
+      maxWidth: 250
+    });
+    var mapDiv = document.getElementById('map');
+    var options = {
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+        position: google.maps.ControlPosition.TOP_CENTER
+      },
+      zoomControl: true,
+      zoomControlOptions: {
+          position: google.maps.ControlPosition.LEFT_CENTER
+      },
+      scaleControl: true,
+      streetViewControl: true,
+      streetViewControlOptions: {
+          position: google.maps.ControlPosition.LEFT_TOP
+      },
+      fullscreenControl: true,
+      styles: map_styles
+    };
+
+    map = new google.maps.Map(mapDiv, options);
+    var bounds = new google.maps.LatLngBounds();
+    var markerInfo = <?php echo json_encode($emparray, JSON_UNESCAPED_UNICODE); ?>;
+
+    for (var i=0; i<markerInfo.length; i++) {
+      var info = markerInfo[i];
+      createMarker(info, map, bounds);
+    }
+    google.maps.event.addListener(map, 'click', function() {
+      if (infoWindow) {
+        infoWindow.close();
+      }
+    });
+    // set max zoom
+    google.maps.event.addListenerOnce(map, 'bounds_changed', function(event) {
+      if (this.getZoom() > 15) {
+        this.setZoom(15);
+      }
+      google.maps.event.addListener(map, 'zoom_changed', function() {
+        var zoom = map.getZoom();
+        // iterate over markers and call setVisible
+        for (i = 0; i < allMarkers.length; i++) {
+          allMarkers[i].setVisible(zoom > 14);
+        }
+      });
+    });
+    map.fitBounds(bounds);
+  }
+  $('form').submit(function(e){
+    e.preventDefault();
+    if (document.forms["gis_form"]["to"].value != '' && document.forms["gis_form"]["from"].value) {
+        $('#sbutton').attr('disabled', 'disabled');
+        $('#spinner').show();
+        var service = new google.maps.DirectionsService();
+        var req = {origin: document.forms["gis_form"]["from"].value,
+            destination: document.forms["gis_form"]["to"].value,
+            travelMode: 'DRIVING'
+        }
+        var directionsDisplay = new google.maps.DirectionsRenderer();
+        directionsDisplay.setMap(map);
+        service.route(req, function(results, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(results);
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+            $('#sbutton').removeAttr('disabled');
+            $('#spinner').hide();
+        });
+    }
+    return false;
+});
+</script>
+<script async defer type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyAIR855uWcQY1sCz2e1Xy72vma3k72Sdzs&language=vi&region=VN&callback=initMap"></script>
+</body>
+</html>
